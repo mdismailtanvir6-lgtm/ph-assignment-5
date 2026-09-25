ea5783d766039cb70be37f4a35660c887d9a8706
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-09-25 09:21:46 +0600

    initial setup done!

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
